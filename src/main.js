28e524341a3ce547fdae695da9d6eff84831a19b
张琴 <zhangqin3@cmhk.com>
2021-02-02 16:41:32 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+import { Toast } from 'vant';
+// 引入jsoneditor css
+// import('jsoneditor/src/scss/jsoneditor.scss');
+
+Vue.use(Vant);
+Vue.use(Toast);
+Vue.prototype.$toast = Toast;
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
